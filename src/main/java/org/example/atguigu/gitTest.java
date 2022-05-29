f2527b64583d830dfd0d85337ff1ec5b385c27f6
zhengxin <zhengxin@atguigu.com>
2022-05-29 15:23:58 +0800

    push commit

--- src/main/java/org/example/atguigu/gitTest.java
+++ src/main/java/org/example/atguigu/gitTest.java
@@ -4,6 +4,8 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("push git");
+
     }
 
 
