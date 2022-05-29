f0da64bc03d624d061c37783eecaa58247cb2da3
zhengxin <zhengxin@atguigu.com>
2022-05-29 13:34:10 +0800

    second commit

--- src/main/java/org/example/atguigu/gitTest.java
+++ src/main/java/org/example/atguigu/gitTest.java
@@ -3,6 +3,7 @@ package org.example.atguigu;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 
 
